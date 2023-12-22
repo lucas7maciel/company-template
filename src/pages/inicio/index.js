c8d1213523547135715a6e7d5a6d954116fa612d
Lucas Maciel <lucasmacielcontato@gmail.com>
2023-12-22 02:01:28 -0300

    Formatei os modulos css

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -3,13 +3,12 @@ import { Section2 } from "./section2/index";
 import { Section3 } from "./section3/index";
 import { Section4 } from "./section4/index";
 import { useRef } from "react";
-import "./index.css";
 
 export const Inicio = () => {
   const sec2Ref = useRef();
 
   return (
-    <div className="page">
+    <div style={{width: "100%"}}>
       <Section1 sec2={sec2Ref} />
       <Section2 reference={sec2Ref} />
       <Section3 />
