e9408855482d350adb7b3baa9a8debfb998728a3
Lucas Maciel <lucasmacielcontato@gmail.com>
2024-01-15 16:07:19 -0300

    Feat: Copy communication values and open social media links

--- src/pages/contato/subcomponents/meio.js
+++ src/pages/contato/subcomponents/meio.js
@@ -2,7 +2,10 @@ import styles from "../index.module.css";
 
 export const Meio = (props) => (
   <div className={styles.meio}>
-    <img src={props.icon} />
+    <img 
+      src={props.icon}
+      onClick={() => navigator.clipboard.writeText(props.value)}
+      title={props.title} />
     <div>
       <p className={styles[`text-${props.order}`]}>{props.value}</p>
     </div>
